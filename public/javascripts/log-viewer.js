6eab8488bd366b47b173a5604b7609e203a81cc9
riccardosl <riccardostoppani@gmail.com>
2021-05-25 22:23:49 +0200

    first commit

---
+++ public/javascripts/log-viewer.js
@@ -0,0 +1,17 @@
+const connection = new WebSocket("ws://localhost:3000");
+const logWindow = document.querySelector("#log-window");
+const filePath = document.getElementById("logFilePath").value;
+
+connection.onopen = () => {
+  if (filePath) {
+    connection.send(filePath)
+  }
+}
+
+
+
+connection.onmessage = (event) => {
+  const logs = event.data.split("\n").join("<hr>")
+  logWindow.innerHTML = logs
+
+}
